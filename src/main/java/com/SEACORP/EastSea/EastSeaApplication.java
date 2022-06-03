b0de8041bc6629f0cf730a446135f2cff559ed23
TBlais10 <blaislighting@gmail.com>
2022-06-02 21:25:08 -0400

    Initial commit

---
+++ src/main/java/com/SEACORP/EastSea/EastSeaApplication.java
@@ -0,0 +1,13 @@
+package com.SEACORP.EastSea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EastSeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EastSeaApplication.class, args);
+	}
+
+}
